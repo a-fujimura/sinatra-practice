5d5f36c92a222e45e4a5ce8d170896e7a2fd63da
a-fujimura <akr9915@gmail.com>
2025-06-04 16:55:32 +0900

    rubocop

--- Gemfile
+++ Gemfile
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
-source "https://rubygems.org"
+source 'https://rubygems.org'
 
 # gem "rails"
-gem "rubocop-fjord"
-gem "erb_lint"
-gem "sinatra"
-gem "json"
+gem 'erb_lint'
+gem 'json'
+gem 'rubocop-fjord'
+gem 'sinatra'
--- app.rb
+++ app.rb
@@ -1,43 +1,39 @@
-require "sinatra"
-require "json"
+# frozen_string_literal: true
 
-$save_file = "memos.json"
+require 'sinatra'
+require 'json'
 
-get "/" do
-  @memos = get_memos()
-  if @memos
-    erb :index
-  end
+SAVE_FILE = 'memos.json'
+
+get '/' do
+  @memos = memos
+  erb :index if @memos
 end
 
-get "/edit/:id" do
+get '/edit/:id' do
   @memo = get_memo(:id)
-  if @memo
-    erb :edit
-  end
+  erb :edit if @memo
 end
 
-get "/new" do
+get '/new' do
   erb :new
 end
 
-get "/show/:id" do
+get '/show/:id' do
   @memo = get_memo(:id)
-  if @memo
-    erb :show
-  end
+  erb :show if @memo
 end
 
-def get_memos()
-  return read_json()[:memos]
+def memos
+  read_json[:memos]
 end
 
 def get_memo(id)
-  return get_memos().find { |i| i[:id] == params[:id].to_i }
+  memos.find { |i| i[id] == params[id].to_i }
 end
 
 def add_memo(memo)
-  memo_json = read_json()
+  memo_json = read_json
   value = memo_json[:autoincrement].to_i + 1
 
   memo[:id] = value
@@ -49,7 +45,7 @@ def add_memo(memo)
 end
 
 def edit_memo(memo)
-  memo_json = read_json()
+  memo_json = read_json
 
   target = memo_json[:memos].find { |x| x[:id] == memo[:id] }
   if target
@@ -61,72 +57,63 @@ def edit_memo(memo)
 end
 
 def delete_memo(memo_id)
-  memo_json = get_memo_json()
+  memo_json = read_json
 
-  if memo_json[:memos]
-    memo_json[:memos].reject! { |x| x[:id].to_i == memo_id.to_i }
-  end
+  memo_json[:memos]&.reject! { |x| x[:id].to_i == memo_id.to_i }
 
   write_json(memo_json)
 end
 
 # 取得(全てのアイテム)
-get "/api/memos" do
+get '/api/memos' do
   content_type :json
   get_memo.to_json
 end
 
 # 取得(指定のアイテム)
-get "/api/memos/:id" do
+get '/api/memos/:id' do
   content_type :json
   memo = get_memo(:id)
-  if memo.to_json
-    memo.to_json
-  else
-    { error: "memo not fount" }.to_json
-  end
+  memo.to_json || { error: 'memo not fount' }.to_json
 end
 
 # 追加
-post "/api/memos/add" do
+post '/api/memos/add' do
   title = params[:title]
   content = params[:content]
 
   new_memo = {
     id: -1,
     title: title,
-    content: content,
+    content: content
   }
   add_memo(new_memo)
 
-  redirect "/"
+  redirect '/'
 end
 
 # 編集
-patch "/api/memos/edit/:id" do
+patch '/api/memos/edit/:id' do
   memo = { id: params[:id].to_i, title: params[:title], content: params[:content] }
   edit_memo(memo)
 
-  redirect "/"
+  redirect '/'
 end
 
 # 削除
-delete "/api/memos/delete/:id" do
+delete '/api/memos/delete/:id' do
   delete_memo(params[:id])
-  redirect "/"
+  redirect '/'
 end
 
 def write_json(memos)
-  File.open($save_file, "w") do |file|
+  File.open(SAVE_FILE, 'w') do |file|
     file.write(JSON.pretty_generate(memos))
   end
 end
 
-def read_json()
-  unless File.exist?($save_file)
-    write_json({ autoincrement: 0, memos: [] })
-  end
+def read_json
+  write_json({ autoincrement: 0, memos: [] }) unless File.exist?(SAVE_FILE)
   # Jsonを取得
-  memos = JSON.parse(File.read($save_file), symbolize_names: true)
-  return memos
+  JSON.parse(File.read(SAVE_FILE), symbolize_names: true)
 end
