992e827bc5747ee66e33a361b8b095dc03985409
a-fujimura <akr9915@gmail.com>
2025-06-17 14:29:53 +0900

    xss対策

--- app.rb
+++ app.rb
@@ -6,6 +6,12 @@ set :erb, escape_html: true
 
 SAVE_FILE = 'memos.json'
 
+helpers do
+  def h(text)
+    ERB::Util.html_escape(text)
+  end
+end
+
 get '/' do
   @memos = memos || []
   erb :index if @memos
